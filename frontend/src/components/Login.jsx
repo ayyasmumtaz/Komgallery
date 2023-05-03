77ebcf3515ba36752cf015b7fe5599682602a4c6
ayyasmumtaz <yazmumtaz@gmail.com>
2023-05-03 08:12:50 +0700

    update logo

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
       <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-black/50">
         <div className="flex flex-col justify-center items-center co">
           <div className="p-5">
-            <img src={logoNs} width="110px" alt="logo" />
+            <img src={logoNs} width="160px" alt="logo" />
           </div>
 
           <div className="shadow-2x1">
